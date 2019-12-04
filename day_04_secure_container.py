9f4fac75fa5b5bd135e27e141f1b050559431b00
Benjamin Watson <benjaminianwatson@gmail.com>
2019-12-04 11:09:32 +0000

    Added day 4.

---
+++ day_04_secure_container.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+
+def part_1(data):
+    count = 0
+    start, stop = map(int, data.split("-"))
+    for i in range(start, stop):
+        string = str(i)
+        if "".join(sorted(string)) != string:
+            continue
+        if any(v > 1 for v in Counter(string).values()):
+            count += 1
+    return count
+
+
+def part_2(data):
+    count = 0
+    start, stop = map(int, data.split("-"))
+    for i in range(start, stop):
+        string = str(i)
+        if "".join(sorted(string)) != string:
+            continue
+        if 2 in Counter(string).values():
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    with open('day_04_input.txt', 'r') as f:
+        inp = f.read()
+        print("Part 1 answer: " + str(part_1(inp)))
+        print("Part 2 answer: " + str(part_2(inp)))
